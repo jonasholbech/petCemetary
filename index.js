7e25cb0de8358e30e5f16dbd20c7d8dc9702d450
Jonas Holbech <jh@jonasholbech.dk>
2023-10-11 09:13:30 +0200

    næsten fuld crud

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-import { getPets, deletePet } from "./utils/petsdb.js";
+import { getPets, deletePet, updatePet } from "./utils/petsdb.js";
 
-async function init() {
+async function showPets() {
   const pets = await getPets();
   console.log(pets);
+  console.log(pets.length, "dyr i databasen");
+  document.querySelector(".pets").innerHTML = "";
+  pets.forEach((pet) => {
+    const template = document.querySelector("template").content;
+    const copy = template.cloneNode(true);
+    copy.querySelector("h2").textContent = pet.name;
+    copy.querySelector(".species").textContent = pet.species;
+    copy.querySelector(".race").textContent = pet.race;
+    copy.querySelector(".status").textContent = pet.isAlive;
+    const deleteButton = copy.querySelector("button[data-action='delete']");
+    deleteButton.dataset.id = pet.id;
+    const updateButton = copy.querySelector("button[data-action='update']");
+    updateButton.dataset.id = pet.id;
+
+    deleteButton.addEventListener("click", async (e) => {
+      await deletePet(pet.id);
+      showPets();
+    });
+    updateButton.addEventListener("click", async (e) => {
+      console.log(pet.id, "skal opdateres");
+      await updatePet(pet.id);
+      showPets();
+    });
+    document.querySelector(".pets").appendChild(copy);
+  });
 }
 
-document.querySelector("button").addEventListener("click", () => {
-  deletePet(6);
-  init();
-});
-init();
+showPets();
--- utils/petsdb.js
+++ utils/petsdb.js
@@ -1,8 +1,10 @@
 const myApiKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3cndwdGlib3RseGx2Y2RlaWN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ3OTI4MDYsImV4cCI6MTk4MDM2ODgwNn0.FuHj1T6qJO-wQ_aWaaXNFVfZPG45FsnE3RvHd3PGQmA";
+
 export async function getPets() {
   let headersList = {
-    apikey: myApiKey,
+    apikey:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3cndwdGlib3RseGx2Y2RlaWN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ3OTI4MDYsImV4cCI6MTk4MDM2ODgwNn0.FuHj1T6qJO-wQ_aWaaXNFVfZPG45FsnE3RvHd3PGQmA",
     Accept: "application/json",
   };
 
@@ -17,10 +19,14 @@ export async function getPets() {
   let data = await response.json();
   return data;
 }
+
 export async function deletePet(id) {
+  console.log("sletter nu:", id);
   let headersList = {
-    apikey: myApiKey,
+    apikey:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3cndwdGlib3RseGx2Y2RlaWN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ3OTI4MDYsImV4cCI6MTk4MDM2ODgwNn0.FuHj1T6qJO-wQ_aWaaXNFVfZPG45FsnE3RvHd3PGQmA",
     Accept: "application/json",
+    Prefer: "return=representation",
   };
 
   let response = await fetch(
@@ -31,14 +37,16 @@ export async function deletePet(id) {
     }
   );
 
-  /* let data = await response.json();
-  console.log(data); */
+  let data = await response.json();
+  return data;
 }
 
 export async function addPet() {
   let headersList = {
-    apikey: myApiKey,
+    apikey:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3cndwdGlib3RseGx2Y2RlaWN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ3OTI4MDYsImV4cCI6MTk4MDM2ODgwNn0.FuHj1T6qJO-wQ_aWaaXNFVfZPG45FsnE3RvHd3PGQmA",
     Accept: "application/json",
+    Prefer: "return=representation",
     "Content-Type": "application/json",
   };
 
@@ -58,22 +66,23 @@ export async function addPet() {
   );
 
   let data = await response.json();
-  console.log(data);
+  return data;
 }
 
-export async function updatePet() {
+export async function updatePet(id) {
   let headersList = {
-    apikey: myApiKey,
+    apikey:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3cndwdGlib3RseGx2Y2RlaWN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ3OTI4MDYsImV4cCI6MTk4MDM2ODgwNn0.FuHj1T6qJO-wQ_aWaaXNFVfZPG45FsnE3RvHd3PGQmA",
+    Prefer: "return=representation",
     "Content-Type": "application/json",
   };
 
   let bodyContent = JSON.stringify({
-    race: "Golden Retriever",
-    species: "Hund",
+    isAlive: false,
   });
 
   let response = await fetch(
-    "https://uwrwptibotlxlvcdeicv.supabase.co/rest/v1/pets?id=eq.6",
+    "https://uwrwptibotlxlvcdeicv.supabase.co/rest/v1/pets?id=eq." + id,
     {
       method: "PATCH",
       body: bodyContent,
@@ -82,5 +91,5 @@ export async function updatePet() {
   );
 
   let data = await response.json();
-  console.log(data);
+  return data;
 }
